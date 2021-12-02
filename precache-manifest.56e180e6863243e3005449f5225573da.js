5fdf7247a7db0bab763248797ec4cb3932d77002
halomai <1581528151@qq.com>
2021-12-02 11:34:33 +0800

    Add files via upload

---
+++ precache-manifest.56e180e6863243e3005449f5225573da.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "61dcd523093e096cbddd",
+    "url": "/static/css/main.0a5b46b2.chunk.css"
+  },
+  {
+    "revision": "61dcd523093e096cbddd",
+    "url": "/static/js/main.46948f4d.chunk.js"
+  },
+  {
+    "revision": "3978adb7fc73116c2f8a",
+    "url": "/static/js/runtime~main.b564e688.js"
+  },
+  {
+    "revision": "b690c2c046e8ecc5ee79",
+    "url": "/static/css/2.610810a6.chunk.css"
+  },
+  {
+    "revision": "b690c2c046e8ecc5ee79",
+    "url": "/static/js/2.820a805f.chunk.js"
+  },
+  {
+    "revision": "a05c318ddc5c1536ba29",
+    "url": "/static/js/3.1bea3a34.chunk.js"
+  },
+  {
+    "revision": "d847480b957dd73c8e7bcb8346799d6e",
+    "url": "/static/media/lab1.d847480b.jpg"
+  },
+  {
+    "revision": "504c8c3cd54cddbbf588b62deefa7068",
+    "url": "/static/media/scut2.504c8c3c.jpg"
+  },
+  {
+    "revision": "c171cd0dac7832e8c3ea97a7e1cfaefd",
+    "url": "/static/media/gd1.c171cd0d.jpg"
+  },
+  {
+    "revision": "46b7725726433a3162d9ed075a5a4ff3",
+    "url": "/static/media/competition_1.46b77257.jpg"
+  },
+  {
+    "revision": "ebd600f53faf255a2536e72073f7662a",
+    "url": "/static/media/logo_left.ebd600f5.png"
+  },
+  {
+    "revision": "5293ef6f215ce89e91b5316f55924082",
+    "url": "/static/media/logo_right.5293ef6f.jpg"
+  },
+  {
+    "revision": "3c9aa8f43a4a83953355192d3fde5095",
+    "url": "/index.html"
+  }
+];
